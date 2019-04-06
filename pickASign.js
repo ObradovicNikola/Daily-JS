28c9d8743304ec0cf99f47390fec77e4a4c482c7
Obradovic Nikola <obradovicnikola009@gmail.com>
2019-04-06 12:37:14 +0200

    optimized solution

--- pickASign.js
+++ pickASign.js
@@ -1,4 +1,4 @@
-// brute force solution
+/* brute force solution
 function numSignWays(arr, sum) {
     return findWays(arr, sum, 0, 0)
 }
@@ -12,5 +12,27 @@ function findWays(arr, sum, p, currSum) {
         return 1;
     return 0;
 }
+*/
+// optimization with dynamic programming
+function numSignWays(arr, sum) {
+    return findWays(arr, sum, 0, 0, {})
+}
 
+function findWays(arr, sum, p, currSum, hash) {
+    const key = p - currSum;
+    if(hash.hasOwnProperty(key))
+        return hash[key];
+    if (p < arr.length) {
+        const positive = findWays(arr, sum, p + 1, currSum + arr[p], hash);
+        const negative = findWays(arr, sum, p + 1, currSum - arr[p], hash);
+        const totalMatches = positive + negative;
+        hash[key] = totalMatches;
+        return totalMatches;
+    }
+    if (currSum == sum)
+        return 1;
+    return 0;
+}
+
+console.log(numSignWays([1, 1, 1], 3));
 console.log(numSignWays([2, 1, 3, 2], 2));
