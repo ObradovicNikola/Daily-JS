54bbfc6822a82d49b7d12555073f19ab77f05545
Obradovic Nikola <obradovicnikola009@gmail.com>
2019-04-07 17:20:37 +0200

    Initial commit

---
+++ sumOfPerfectSquares.js
@@ -0,0 +1,23 @@
+function howManySquares(n) {
+    return helper(n, {});
+}
+
+function helper(n, hash) {
+    if (hash.hasOwnProperty(n))
+        return hash[n];
+    if (n <= 3)
+        return n;
+    res = n;
+    for (let i = 2; i < n + 1; i++) {
+        tmp = i * i;
+        if (tmp > n) {
+            break;
+        } else {
+            res = Math.min(res, 1 + helper(n - tmp, hash));
+        }
+    }
+    hash[n] = res;
+    return res;
+}
+
+console.log(howManySquares(100));
